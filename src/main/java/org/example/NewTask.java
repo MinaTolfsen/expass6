68d752371616cc82f66829ce6f836fa61aab2de6
Mina Tolfsen <minabye@me.com>
2024-10-01 14:11:40 +0200

    done

--- src/main/java/org/example/NewTask.java
+++ src/main/java/org/example/NewTask.java
@@ -1,25 +1,28 @@
 package org.example;
 
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
-import java.nio.charset.StandardCharsets;
+public class NewTask {
 
-public class Send {
-
-    private final static String QUEUE_NAME = "hello";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = String.join(" ", argv);
+
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
+
 }
